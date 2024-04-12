57d5f97dc58bdca6bb66bc43d82334589e445f56
Velkog <thevelkog@gmail.com>
2024-04-12 16:50:59 -0700

    Add executable option to python lint helper

--- tools/lint/lint.py
+++ tools/lint/lint.py
@@ -1,7 +1,33 @@
+#!/usr/bin/env python
+"""
+A wrapper script used for executing a given lint/format executable
+across multiple files and to use for continuous integration.
+"""
+
+from argparse import ArgumentParser, Namespace
+from shutil import which
 from sys import exit
 
+def parse_args() -> None:
+    parser = ArgumentParser(description=__doc__)
+    parser.add_argument(
+        "--lint-executable",
+        metavar="EXECUTABLE",
+        help="path to the lint executable",
+        required=True)
+    return parser.parse_args()
+
+def validate_args(args: Namespace) -> None:
+    if not which(args.lint_executable):
+        raise NameError(f"Given executable '{args.lint_executable}' does not exist.")
+
 def main() -> None:
-    print("Hello, world!")
+    try:
+        args = parse_args()
+        validate_args(args)
+    except Exception as e:
+        print(e)
+        raise e
 
 if __name__ == '__main__':
     exit(main())
